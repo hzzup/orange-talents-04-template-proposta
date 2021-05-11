24543e5a31d25207d965fbfd45f74ec30624f724
Hzc <humberto.costa@zup.com.br>
2021-05-11 14:21:55 -0300

    Autenticacao com keycloak

---
+++ src/main/java/br/com/hzup/desafioproposta/compartilhado/seguranca/SecurityConfig.java
@@ -0,0 +1,22 @@
+package br.com.hzup.desafioproposta.compartilhado.seguranca;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests(authorizeRequests -> authorizeRequests
+				.antMatchers(HttpMethod.GET, "/propostas/**").hasAuthority("SCOPE_acesso_cartao")
+				.antMatchers(HttpMethod.GET, "/api/cartoes/**").hasAuthority("SCOPE_acesso_cartao")
+				.antMatchers(HttpMethod.POST, "/api/cartoes/**").hasAuthority("SCOPE_acesso_proposta")
+				.antMatchers(HttpMethod.POST, "/propostas/**").hasAuthority("SCOPE_sistema_geral")
+				.anyRequest().authenticated())
+				.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+	}
+}
